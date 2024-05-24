91d384d46c2a846276c2fbf82c5db3e94e0dba48
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-24 07:51:49 -0700

    changed connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://avilwock:Arpeggio@cluster0.gay9utb.mongodb.net/BookSearchEngine?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'), {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+};
 
 module.exports = mongoose.connection;
