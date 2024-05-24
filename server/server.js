c37136d039c07da3f0c11e8bf0e0359b1dc93534
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-24 07:59:20 -0700

    mayve fixed server

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ async function startApolloServer() {
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
   // Apply Apollo Server middleware to the Express app
   
@@ -43,3 +47,4 @@ if (process.env.NODE_ENV === "production") {
 }
 
 startApolloServer();
+
