f444040a8f27bb93f700eef7dacd80cfefc8b92c
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-24 07:29:28 -0700

    updated config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://avilwock:Arpeggio@cluster0.gay9utb.mongodb.net/BookSearchEngine?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
